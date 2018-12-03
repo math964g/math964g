485e970dd53eada15f18386f19b40d3ae4b51c57
math964g <math964g@edu.eal.dk>
2018-12-03 09:07:31 +0100

    Added things

---
+++ Ex32-LinkedList/Adt/MyLinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adt
+{
+    class MyLinkedList
+    {
+        private Node Head;
+        public int Count;   
+
+        public void Insert(object)
+        {
+
+        }
+
+        public void Insert(object, index)
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void Delete(index)
+        {
+
+        }
+
+        public object ItemAt(index)
+        {
+
+        }
+
+        public string ToString()
+        {
+
+        }
+    }
+
+    public class Node
+    {
+        public Node Next;
+        public object Data;
+
+        public Node(object)
+        {
+
+        }
+    }
+
+}
---
+++ Ex32-LinkedList/Ex32-LinkedList/ClubMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adt;
+
+namespace Ex32_LinkedList
+{
+    class ClubMember
+    {
+        public int nr;
+        public string fName;
+        public string lName;
+        public int age;
+
+        public ClubMember ClubMember(nr, fName, lName, age)
+        {
+            Nr = 0;
+        }
+
+        public string ToString()
+        {
+
+        }
+    }
+}
